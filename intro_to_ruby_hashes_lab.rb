8f1f91032f806e7e19739b9419f4d681e1204062
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-26 22:56:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,7 +54,9 @@ key = {
       :reading_railroad => {
         :mortgage_value => 100
       },
-      :pennsylvania_railroad => {},
+      :pennsylvania_railroad => {
+        :mortgage_value => 200
+      },
       :b_and_o_railroad => {},
       :shortline_railroad => {}
     }
